d69d959a97a1687ebc008ec74fef328da671f240
raymondchen <haojunchen51@gmail.com>
2022-07-21 13:16:02 -0700

    server set up

--- testApp/settings.py
+++ testApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_%&qo38qarqa&usq^t@a*^6*#q^)_fg0k9o0m$_)yhtywm&_-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'musicsurveyapp.herokuapp.com/']
 
 
 # Application definition
